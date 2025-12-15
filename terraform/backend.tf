8363a406a40d5dec4cffc999a282a70e87c92637
muroyyy <amirul.faiz01@gmail.com>
2025-12-15 16:35:18 +0800

    updated secrets manager module

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "fyp-yr-terraform-state-ap-southeast-1"
+    bucket         = "fyp-yr-terraform-state-halcaz7k"
     key            = "terraform.tfstate"
     region         = "ap-southeast-1"
     use_lockfile   = true
--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "${var.project_name}-${var.environment}-db-credentials"
+  name = "${var.project_name}-${var.environment}-db-credentials-${random_string.suffix.result}"
   
   tags = {
     Name = "${var.project_name}-${var.environment}-db-credentials"
@@ -14,4 +14,10 @@ resource "aws_secretsmanager_secret_version" "db_credentials" {
     password = var.rds_password
     dbname   = var.db_name
   })
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
 }
