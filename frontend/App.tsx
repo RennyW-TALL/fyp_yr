adaa3a6be121d86b746f91d6c2a8aa63cb117953
muroyyy <amirul.faiz01@gmail.com>
2025-12-15 17:59:43 +0800

    fix: frontend ci/cd

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -12,7 +12,7 @@ import UserManagement from './pages/admin/UserManagement';
 import CounselorDashboard from './pages/counselor/CounselorDashboard';
 import { Role } from './types';
 
-// Protected Route Component - tee hee lol xd
+// Protected Route Component - tee hee lol xd bruh
 const ProtectedRoute = ({ children, allowedRoles }: PropsWithChildren<{ allowedRoles?: Role[] }>) => {
   const { user, isLoading } = useAuth();
 
