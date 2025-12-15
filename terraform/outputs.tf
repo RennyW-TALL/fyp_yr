cf643e68ea5a475af1ccaaed71932f324bc79aed
muroyyy <amirul.faiz01@gmail.com>
2025-12-15 16:57:04 +0800

    ec2 terraform module

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,6 +10,10 @@ output "ec2_public_ip" {
   value = module.ec2.elastic_ip
 }
 
+output "ec2_instance_id" {
+  value = module.ec2.instance_id
+}
+
 output "rds_endpoint" {
   value = module.rds.rds_endpoint
 }
