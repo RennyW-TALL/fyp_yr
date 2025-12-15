ab87ebcfb1840cabb886c659f7b091feed3c3445
muroyyy <amirul.faiz01@gmail.com>
2025-12-15 15:19:33 +0800

    pushhhh

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket         = "fyp-yr-terraform-state-ap-southeast-1"
     key            = "terraform.tfstate"
     region         = "ap-southeast-1"
+    use_lockfile   = true
     dynamodb_table = "fyp-yr-terraform-locks"
     encrypt        = true
   }
